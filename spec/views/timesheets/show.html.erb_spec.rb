0960cdeea9b73d244fb2907aa30443354d7d672a
Ben Ratelade <ratelade.benjamin@gmail.com>
2022-03-12 14:05:53 +1100

    Added total to show timesheet view.

--- spec/views/timesheets/show.html.erb_spec.rb
+++ spec/views/timesheets/show.html.erb_spec.rb
@@ -11,11 +11,44 @@ describe "timesheets/show", type: :view do
       "A Timesheet", 
       start_date: Date.parse("24 Jan 2022"), 
       end_date: Date.parse("30 Jan 2022"),
+      total_hours_worked: "",
     )
     render
 
     expect(rendered).to have_css("h2", text: "Timesheet for Monday, January 24 2022 to Sunday, January 30 2022")
   end
 
-  it "Displays "
+  it "Displays the total number of hours worked for that timesheet" do
+    line_items = [
+      double(
+        "line item 1", 
+        start_time: Time.zone.parse("Jan 31 2022 08:00am"),
+        end_time: Time.zone.parse("Jan 31 2022 17:00"),
+      ), 
+      double(
+        "line item 2", 
+        start_time: Time.zone.parse("Feb 01 2022 08:00am"),
+        end_time: Time.zone.parse("Feb 01 2022 17:00"),
+      ), 
+      double(
+        "line item 3", 
+        start_time: Time.zone.parse("Feb 02 2022 08:00am"),
+        end_time: Time.zone.parse("Feb 02 2022 17:00"),
+      )
+    ]
+
+    @timesheet = double(
+      start_date: Date.parse("24 Jan 2022"), 
+      end_date: Date.parse("30 Jan 2022"),
+      line_items: line_items, 
+      total_hours_worked: "27",
+    )
+
+    render
+
+    page = Capybara.string(rendered)
+    total = page.find("#total-section #value")
+    expect(total.text).to eq("27")
+
+  end
 end

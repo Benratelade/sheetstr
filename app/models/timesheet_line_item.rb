e26b2abbcce0a95d47d2d550cd321952db8e569b
Ben Ratelade <ratelade.benjamin@gmail.com>
2022-03-13 15:36:02 +1100

    Added hours calculations on timesheet_line_items.

--- app/models/timesheet_line_item.rb
+++ app/models/timesheet_line_item.rb
@@ -1,10 +1,28 @@
 class TimesheetLineItem < ApplicationRecord
   belongs_to :timesheet
 
-  def total_hours_worked
+  def total_decimal_hours
     return 0 if (start_time.blank? || end_time.blank?)
 
     seconds = end_time - start_time
     seconds / 3600 
+  end
+
+  def hours_breakdown
+    breakdown = {
+      hours: 0,
+      minutes: 0 
+    }
+
+    return breakdown if (start_time.blank? || end_time.blank?)
+    seconds = end_time - start_time
+    minutes = seconds / 60
+    hours = minutes / 60
+    remaining_minutes = minutes % 60
+
+    breakdown[:hours] = hours.to_i
+    breakdown[:minutes] = remaining_minutes.to_i
+
+    breakdown
   end
 end
--- spec/models/timesheet_line_item_spec.rb
+++ spec/models/timesheet_line_item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TimesheetLineItem, type: :model do
         start_time: nil
       )
 
-      expect(line_item.total_hours_worked).to eq(0)
+      expect(line_item.total_decimal_hours).to eq(0)
     end
 
     it "returns 0 if end_time is nil" do 
@@ -15,7 +15,7 @@ RSpec.describe TimesheetLineItem, type: :model do
         end_time: nil
       )
 
-      expect(line_item.total_hours_worked).to eq(0)
+      expect(line_item.total_decimal_hours).to eq(0)
     end
 
     it "returns the number of decimal hours between the start_time and end_time" do
@@ -24,7 +24,7 @@ RSpec.describe TimesheetLineItem, type: :model do
         end_time: Time.zone.parse("Jan 31 2022 17:30pm"),
       )
 
-      expect(line_item.total_hours_worked).to eq(9.5)
+      expect(line_item.total_decimal_hours).to eq(9.5)
     end
 
     it "returns the number of decimal hours between the start_time and end_time, down to 2 decimals" do
@@ -33,7 +33,7 @@ RSpec.describe TimesheetLineItem, type: :model do
         end_time: Time.zone.parse("Jan 31 2022 14:33pm"),
       )
 
-      expect(line_item.total_hours_worked).to eq(6.55)
+      expect(line_item.total_decimal_hours).to eq(6.55)
     end
 
     it "doesn't mind if start_time and end_time are on different dates" do
@@ -42,7 +42,43 @@ RSpec.describe TimesheetLineItem, type: :model do
         end_time: Time.zone.parse("Feb 01 2022 14:33pm"),
       )
 
-      expect(line_item.total_hours_worked).to eq(30.55)
+      expect(line_item.total_decimal_hours).to eq(30.55)
+    end
+  end
+
+  describe "#hours_breakdown" do
+    it "returns a hash with 0 hours and 0 minutes when there is no start_time" do
+      line_item = TimesheetLineItem.new(
+        start_time: nil
+      )
+
+      expect(line_item.hours_breakdown).to eq({
+        hours: 0, 
+        minutes: 0, 
+      })
+    end
+
+    it "returns a hash with 0 hours and 0 minutes when there is no end_time" do
+      line_item = TimesheetLineItem.new(
+        end_time: nil
+      )
+
+      expect(line_item.hours_breakdown).to eq({
+        hours: 0, 
+        minutes: 0, 
+      })
+    end
+
+    it "returns the number of hours and minutes between the start_time and end_time in a hash" do
+      line_item = TimesheetLineItem.new(
+        start_time: Time.zone.parse("Jan 31 2022 08:00am"),
+        end_time: Time.zone.parse("Jan 31 2022 17:30pm"),
+      )
+
+      expect(line_item.hours_breakdown).to eq({
+        hours: 9,
+        minutes: 30,
+      })
     end
   end
 end

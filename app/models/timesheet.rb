f4cd01e87a7f93dd8e80c78c5718ed4c9a45be02
Ben Ratelade <ratelade.benjamin@gmail.com>
2022-03-14 20:03:58 +1100

    Added a method to get the breakdown of hours and minutes on an entire timesheet.

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -5,4 +5,23 @@ class Timesheet < ApplicationRecord
   def total_hours_worked
     (line_items || []).sum(&:total_decimal_hours)
   end
+
+  def hours_breakdown
+    breakdown = {
+      hours: 0,
+      minutes: 0,
+    }
+
+    (line_items || []).each do |line_item|
+      breakdown[:minutes] += line_item.hours_breakdown[:minutes]
+      breakdown[:hours] += line_item.hours_breakdown[:hours]
+    end
+
+    if breakdown[:minutes] >= 60
+      breakdown[:hours] += (breakdown[:minutes]) / 60
+      breakdown[:minutes] = breakdown[:minutes] % 60
+    end
+
+    breakdown
+  end
 end
--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -31,4 +31,56 @@ RSpec.describe Timesheet, type: :model do
       expect(timesheet.total_hours_worked).to eq(12.75)
     end
   end
+
+  describe "#hours_breakdown" do
+    it "returns a hash with 0 hours and 0 minutes when there are no line_items" do
+      timesheet = Timesheet.new()
+      allow(timesheet).to receive(:line_items).and_return(nil)
+
+      expect(timesheet.hours_breakdown).to eq({
+        hours: 0, 
+        minutes: 0, 
+      })
+    end
+
+    it "returns a hash with the sum of all hours and minutes on all line_items" do
+      timesheet = Timesheet.new()
+      line_item_1 = double(
+        "line_item_1", 
+        id: "the id",
+        hours_breakdown: {
+          hours: 7, 
+          minutes: 35,
+        },
+      )
+      line_item_2 = double(
+        "line_item_2", 
+        id: "the id",
+        hours_breakdown: {
+          hours: 7, 
+          minutes: 35,
+        },
+      )
+      allow(timesheet).to receive(:line_items).and_return([line_item_1, line_item_2])
+
+      expect(timesheet.hours_breakdown).to eq(
+        {
+          hours: 15, 
+          minutes: 10, 
+        }
+      )
+    end
+
+    # it "returns the number of hours and minutes between the start_time and end_time in a hash" do
+    #   line_item = TimesheetLineItem.new(
+    #     start_time: Time.zone.parse("Jan 31 2022 08:00am"),
+    #     end_time: Time.zone.parse("Jan 31 2022 17:30pm"),
+    #   )
+
+    #   expect(line_item.hours_breakdown).to eq({
+    #     hours: 9,
+    #     minutes: 30,
+    #   })
+    # end
+  end
 end

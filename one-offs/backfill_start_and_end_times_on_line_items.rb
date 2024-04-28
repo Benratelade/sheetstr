748609e7064ff614f9e977f2b543b84e757cda58
Ben Ratelade <ratelade.benjamin@gmail.com>
2024-04-28 10:07:48 +1000

    Rubocop the one-off

--- one-offs/backfill_start_and_end_times_on_line_items.rb
+++ one-offs/backfill_start_and_end_times_on_line_items.rb
@@ -11,7 +11,7 @@ Timesheet.all.each do |timesheet|
         timezone_identifier: "Sydney",
       ),
       end_time: Utils::DateTimeBuilder.build_from_date_and_time(
-        date: start_date.next_week(line_item.weekday.to_sym) - 7.days,,
+        date: start_date.next_week(line_item.weekday.to_sym) - 7.days,
         time: line_item.end_time.strftime("%X"),
         timezone_identifier: "Sydney",
       ),
@@ -22,10 +22,13 @@ end
 LineItem.all.each do |line_item|
   weekday = line_item.weekday
   calculated_weekday = Utils::DateTimeBuilder.build_from_date_and_time(
-      date: line_item.timesheet.start_date.next_week(line_item.weekday.to_sym) - 7.days,
-      time: line_item.start_time.strftime("%X"),
-      timezone_identifier: "Sydney",
-    ).strftime("%A").downcase
+    date: line_item.timesheet.start_date.next_week(line_item.weekday.to_sym) - 7.days,
+    time: line_item.start_time.strftime("%X"),
+    timezone_identifier: "Sydney",
+  ).strftime("%A").downcase
 
-    ap(line_item: line_item.id, weekday: weekday, calculated_weekday: calculated_weekday) unless weekday == calculated_weekday
+  unless weekday == calculated_weekday
+    ap(line_item: line_item.id, weekday: weekday,
+       calculated_weekday: calculated_weekday,)
+  end
 end

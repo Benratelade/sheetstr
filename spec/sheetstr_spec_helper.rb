5cf1f1008b1b5042778b7370e8fa5b7e72e08585
Ben Ratelade <ratelade.benjamin@gmail.com>
2024-05-05 16:36:19 +1000

    Upgrade defaults. Address deprecation for Devise TestHelpers. Ignore :weekday column

--- spec/sheetstr_spec_helper.rb
+++ spec/sheetstr_spec_helper.rb
@@ -7,7 +7,7 @@ Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include Support::PageFragments
   config.include Support::PageFragments::Login
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Support::Controllers, type: :controller
 end
 

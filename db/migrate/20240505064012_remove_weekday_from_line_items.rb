e4782a966680a955db5c569b4ef347e8ecfb967e
Ben Ratelade <ratelade.benjamin@gmail.com>
2024-05-05 19:25:47 +1000

    Actually, it's better to remove the correct column

--- db/migrate/20240505064012_remove_weekday_from_line_items.rb
+++ db/migrate/20240505064012_remove_weekday_from_line_items.rb
@@ -1,5 +1,5 @@
 class RemoveWeekdayFromLineItems < ActiveRecord::Migration[7.1]
   def change
-    remove_column :line_items, :hourly_rate
+    remove_column :line_items, :weekday
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_28_043805) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_064012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_28_043805) do
     t.uuid "timesheet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.enum "weekday", enum_type: "weekdays"
     t.integer "hourly_rate_cents"
     t.string "currency", default: "XXX"
     t.index ["timesheet_id"], name: "index_line_items_on_timesheet_id"
